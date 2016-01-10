0f955bb236329fbb02f714eb5abba2a9b79f4067
rookiebulls <rookiebulls@163.com>
2016-01-11 00:02:42 +0800

    edit catergory

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.login import LoginManager, login_user, logout_user, UserMixin, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 
 import os
 
@@ -27,21 +28,43 @@ def load_user(id):
 
 
 
+class Catergory(db.Model):
+
+	__tablename__ = 'catergory'
+
+	id = db.Column(db.Integer, primary_key=True)
+	name  = db.Column(db.String(20))
+	posts = db.relationship('Post', backref='catergory', lazy='dynamic')
+
+	# def __init__(self, name):
+		# self.name = name
+
+	# def __repr__(self):
+		# return '<catergory:%r' % self.name
+
+
+
 class Post(db.Model):
 
 	__tablename__ = 'posts'
 
 	id = db.Column(db.Integer, primary_key=True)
-	title = db.Column(db.String, nullable=False)
-	article = db.Column(db.String, nullable=False)
+	title = db.Column(db.String(80))
+	article = db.Column(db.Text)
+	pub_date = db.Column(db.DateTime)
+	catergory_id = db.Column(db.Integer, db.ForeignKey('catergory.id'))
 
 
-	def __init__(self, title, article):
-		self.title = title
-		self.article = article
+	# def __init__(self, title, article):
+		# self.title = title
+		# self.article = article
+		# self.pub_date = datetime.utcnow()
 
 	def __repr__(self):
 		return '<tilele:%r' % self.title
+
+
+
 
 
 class User(UserMixin, db.Model):
@@ -53,26 +76,25 @@ class User(UserMixin, db.Model):
 	password_hash = db.Column(db.String(128), nullable=False)
 
 
-	def __init__(self, username, password, active=True):
-		self.username = username
+	# def __init__(self, username, password, active=True):
+		# self.username = username
 		# self.password = self.set_password(password)
-		self.password_hash = generate_password_hash(password)
-		self.active = active		
+		# self.password_hash = generate_password_hash(password)
+		# self.active = active		
 
 
 	def verify_password(self, password):
 		return check_password_hash(self.password_hash, password)
 
-
 	def is_active(self):
-		return self.active
+		# return self.active
+		return True
 
 	def is_authenticated(self):
 		return True
 
 	def is_anonymous(self):
 		return False
-	
 
 	def __repr__(self):
 		return '<user:%r' % self.username		
@@ -99,7 +121,12 @@ class writeForm(Form):
 def home():
 	posts = Post.query.all()
 	posts = posts[::-1]
-	return render_template('home.html', posts=posts)
+	catergories = []
+	for catergory in Catergory.query.all():
+		post_count = Post.query.filter_by(catergory_id=catergory.id).all()
+		catergories.append((catergory, len(post_count)))
+
+	return render_template('home.html', posts=posts, catergories=catergories)
 
 
 
@@ -133,7 +160,12 @@ def write():
     if request.method == 'POST':
     	title = request.form['title']
     	article = request.form['article']
-    	db.session.add(Post(title, article))
+    	catergory_select = request.form['catergory']
+    	catergory_search = Catergory.query.filter_by(name=catergory_select).first()
+    	if catergory_search:
+	    	db.session.add(Post(title=title, article=article, pub_date=datetime.utcnow(), catergory=catergory_search))
+        else:
+	    	db.session.add(Post(title=title, article=article, pub_date=datetime.utcnow(), catergory=Catergory(name=catergory_select)))
     	db.session.commit()
     	return redirect(url_for('home'))
 
@@ -141,10 +173,29 @@ def write():
 
 
 @app.route('/article/<int:post_id>')
-def article(post_id):
-	posts = Post.query.filter_by(id=post_id).all()
-	return render_template('article.html', posts=posts)
+def article(post_id): 
+	# same_catergory_post_ids = None
+	post = Post.query.filter_by(id=post_id).first()
+	if post:
+		same_catergory_posts = Catergory.query.filter_by(name=post.catergory.name).first()
+		ids = [catergory_post.id for catergory_post in same_catergory_posts.posts]
+		# print same_catergory_post_ids
+	catergories = []
+	for catergory in Catergory.query.all():
+		post_count = Post.query.filter_by(catergory_id=catergory.id).all()
+		catergories.append((catergory, len(post_count)))
+	return render_template('article.html', post=post, catergories=catergories,ids=ids)
 
+
+@app.route('/<catergory_name>')
+def articles_of_catergory(catergory_name):
+	catergories = []
+	for catergory in Catergory.query.all():
+		post_count = Post.query.filter_by(catergory_id=catergory.id).all()
+		catergories.append((catergory, len(post_count)))
+	catergory= Catergory.query.filter_by(name=catergory_name).first()
+	posts_of_catergory = catergory.posts
+	return render_template('catergory.html', posts_of_catergory=posts_of_catergory, catergories=catergories)
 
 
 @app.route('/logout')
--- create_all.py
+++ create_all.py
@@ -1,13 +1,23 @@
 from app import db
-from app import Post, User
+from app import Post, User, Catergory
+from werkzeug.security import generate_password_hash
+from datetime import datetime
 
 db.drop_all()
 db.create_all()
 
-first_art = Post('Hello', 'Hello World')
-admin = User('admin', 'admin')
 
-db.session.add(first_art)
+catergory_one = Catergory(name='Python')
+catergory_two = Catergory(name='Ruby')
+post_one = Post(title='Hello', article='Hello World', pub_date=datetime.utcnow(), catergory=catergory_one)
+post_two = Post(title='Hi', article='Hi There', pub_date=datetime.utcnow(), catergory=catergory_two)
+admin = User(username='admin', password_hash=generate_password_hash('admin'))
+
+
+db.session.add(catergory_one)
+db.session.add(catergory_two)
+db.session.add(post_one)
+db.session.add(post_two)
 db.session.add(admin)
 
 db.session.commit()
